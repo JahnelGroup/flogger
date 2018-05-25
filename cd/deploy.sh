c5bc97d6e1c572998804480e6f526398261834e0
Steven Vickers <svickers@jahnelgroup.com>
2018-05-25 14:23:50 -0400

    trying to fix keys

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn -f flogger-core/pom.xml deploy -P sign --settings cd/mvnsettings.xml
-    mvn -f flogger-spring-boot/pom.xml deploy -P sign --settings cd/mvnsettings.xml
-    mvn -f flogger-sample/pom.xml deploy -P sign --settings cd/mvnsettings.xml
+    mvn -f flogger-core/pom.xml deploy --settings cd/mvnsettings.xml
+    mvn -f flogger-spring-boot/pom.xml deploy --settings cd/mvnsettings.xml
+    mvn -f flogger-sample/pom.xml deploy --settings cd/mvnsettings.xml
 fi
