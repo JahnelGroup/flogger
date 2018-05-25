6a665bb1977302a13475b8e24134b4ba7427987e
Steven Vickers <svickers@jahnelgroup.com>
2018-05-25 13:22:38 -0400

    updating name of signing key

--- cd/gpg-sign.sh
+++ cd/gpg-sign.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_939014b31d23_key -iv $encrypted_939014b31d23_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-gpg --fast-import signingkey.asc
+gpg --fast-import codesigning.asc
