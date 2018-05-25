b3eca749c49304a543e5dd31a603e14a0156ee7a
Steven Vickers <svickers@jahnelgroup.com>
2018-05-25 13:12:59 -0400

    updating paths in scripts

--- cd/gpg-sign.sh
+++ cd/gpg-sign.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_939014b31d23_key -iv $encrypted_939014b31d23_iv -in codesigning.asc.enc -out codesigning.asc -d
-gpg --fast-import cd/signingkey.asc
+gpg --fast-import signingkey.asc
