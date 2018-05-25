223cdc4e44e874e16f87fd57bbbbe23b6f545e59
Steven Vickers <svickers@jahnelgroup.com>
2018-05-25 13:35:57 -0400

    only sign when deploying?

--- cd/gpg-sign.sh
+++ cd/gpg-sign.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_939014b31d23_key -iv $encrypted_939014b31d23_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-gpg --fast-import cd/codesigning.asc
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_939014b31d23_key -iv $encrypted_939014b31d23_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
+fi
