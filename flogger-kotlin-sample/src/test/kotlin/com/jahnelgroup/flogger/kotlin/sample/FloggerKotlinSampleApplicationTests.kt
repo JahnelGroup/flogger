2201b587e3a5d4fe60ab6780cd9c0ed818babf6d
Steven Vickers <svickers@jahnelgroup.com>
2018-05-30 15:13:06 -0400

    removed @BindFields and @BindMethods and simplified to @BindExpand
    added a kotlin test app because spindle wasnt working when seney tried it
    updated to 2.0.0 because the @BindExpand change is a breaking change
    removed flogger-sample from parent pom modules. we dont need to deploy that

---
+++ flogger-kotlin-sample/src/test/kotlin/com/jahnelgroup/flogger/kotlin/sample/FloggerKotlinSampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jahnelgroup.flogger.kotlin.sample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FloggerKotlinSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
